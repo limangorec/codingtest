2ec1584a969b4f6170a10ed58e2e8ea678fa410c
Oliver Müller <Mistpenner@web.de>
2022-03-11 14:05:04 +0100

    coding test done by oliver mueller

--- src/Command/PurchaseCigarettesCommand.php
+++ src/Command/PurchaseCigarettesCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Command;
 
+use App\Machine\CigaretteMachine;
+use App\Model\Transaction;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
@@ -34,23 +36,19 @@ class PurchaseCigarettesCommand extends Command
         $itemCount = (int) $input->getArgument('packs');
         $amount = (float) \str_replace(',', '.', $input->getArgument('amount'));
 
-
-        // $cigaretteMachine = new CigaretteMachine();
-        // ...
-
-        $output->writeln('You bought <info>...</info> packs of cigarettes for <info>...</info>, each for <info>...</info>. ');
-        $output->writeln('Your change is:');
-
-        $table = new Table($output);
-        $table
-            ->setHeaders(array('Coins', 'Count'))
-            ->setRows(array(
-                // ...
-                array('0.02', '0'),
-                array('0.01', '0'),
-            ))
-        ;
-        $table->render();
-
+        $purchaseTransaction = new Transaction($itemCount, $amount);
+        $cigaretteMachine = new CigaretteMachine();
+        $resultOfTransaction = $cigaretteMachine->execute($purchaseTransaction);
+        // check if customer paid enough
+        if($resultOfTransaction['paidEnough']){
+            $output->writeln($resultOfTransaction['message']);
+            $output->writeln('Your change is:');
+            $table = new Table($output);
+            $table->setHeaders(array('Coins', 'Count'))
+                ->setRows($resultOfTransaction['changeTable']);
+            $table->render();
+        } else {
+            $output->writeln($resultOfTransaction['message']);
+        }
     }
 }
